8a2a79e5df46c4c630506bd067074614a62bcafa
Polydick <zdk_cz@sina.com>
2017-09-28 19:18:12 +0800

    first commit

---
+++ src/main/main.ts
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from "electron";
+
+let win: Electron.BrowserWindow = null;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 700,
+        height: 500,
+        show: false
+    });
+    win.loadURL("file://" + __dirname + "/dist/index.html");
+    win.webContents.openDevTools();
+    win.on("closed", () => win = null);
+    win.on("ready-to-show", () => {
+        win.show();
+        win.focus();
+    });
+}
+
+app.on("ready", () => createWindow());
+app.on("window-all-closed", () => process.platform !== "darwin" && app.quit());
+app.on("activate", () => win === null && createWindow());
